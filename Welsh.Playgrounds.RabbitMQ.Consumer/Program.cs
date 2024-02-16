7b9f81def7a01b238b6d738f3fcdd76b912867c8
CassieWelsh <maksim.airapetyan3@yandex.ru>
2024-02-16 23:35:12 +0300

    Add project files.

---
+++ Welsh.Playgrounds.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.BasicQos(prefetchSize: 0, prefetchCount: 2, global: false);
+
+var props = channel.CreateBasicProperties();
+props.Persistent = true;
+
+if (!int.TryParse(args[0], out var processTime))
+    throw new ArgumentException("Process time value must be integer");
+
+const string QUEUE_NAME = "Sample";
+
+channel.QueueDeclare(
+    queue: QUEUE_NAME,
+    exclusive: false,
+    durable: true,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var payload = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(payload);
+    Console.WriteLine($"{message}, {DateTime.Now}");
+    Thread.Sleep(processTime);
+    (sender as EventingBasicConsumer)!.Model.BasicAck(args.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(
+    queue: QUEUE_NAME,
+    autoAck: false,
+    consumer: consumer
+);
+
+Console.ReadLine();
---
+++ Welsh.RabbitMQ.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+const string QUEUE_NAME = "Sample";
+
+channel.QueueDeclare(
+    queue: QUEUE_NAME,
+    exclusive: false,
+    durable: true,
+    autoDelete: false,
+    arguments: null
+);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+using var cts = new CancellationTokenSource();
+
+var props = channel.CreateBasicProperties();
+props.Persistent = true;
+while (!cts.IsCancellationRequested)
+{
+    var message = Console.ReadLine();
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: QUEUE_NAME,
+        basicProperties: props,
+        body: messageBody
+    );
+}
+
+
